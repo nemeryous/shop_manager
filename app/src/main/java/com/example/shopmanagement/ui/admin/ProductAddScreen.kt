e8c1ec5c0a1542472c6da3a1bb3ffba54d1228ca
Mai Phương Nam <nammp.22it@vku.udn.vn>
2024-04-23 20:50:05 +0700

    test

--- app/src/main/java/com/example/shopmanagement/ui/admin/ProductAddScreen.kt
+++ app/src/main/java/com/example/shopmanagement/ui/admin/ProductAddScreen.kt
@@ -1,15 +1,10 @@
 package com.example.shopmanagement.ui.admin
 
-import android.graphics.Bitmap
-import android.graphics.BitmapFactory
-import android.graphics.ImageDecoder
-import android.os.Build
-import android.provider.MediaStore
 import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.activity.result.launch
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -17,18 +12,33 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.foundation.text.KeyboardOptions
-import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.AddAPhoto
+import androidx.compose.material.icons.filled.ArrowBackIosNew
+import androidx.compose.material.icons.filled.Camera
+import androidx.compose.material.icons.filled.CameraAlt
+import androidx.compose.material.icons.filled.Gradient
+import androidx.compose.material.icons.filled.KeyboardArrowDown
+import androidx.compose.material.icons.filled.Photo
+import androidx.compose.material3.Button
+import androidx.compose.material3.DropdownMenu
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedButton
 import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
@@ -37,17 +47,11 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.asImageBitmap
-import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import androidx.core.graphics.scale
-import androidx.lifecycle.viewmodel.compose.viewModel
-import com.example.shopmanagement.AppViewModelProvider
 import com.example.shopmanagement.R
 import com.example.shopmanagement.ui.navigation.NavigationDestination
 
@@ -61,161 +65,180 @@ object ProductAddDestination : NavigationDestination {
 
 @Composable
 fun ProductAddScreen(
-    modifier: Modifier = Modifier,
-    productAddViewModel: ProductAddViewModel = viewModel(factory = AppViewModelProvider.Factory)
 ) {
-
-    val productAddUiState by productAddViewModel.productAddUiState.collectAsState()
-
-    val context = LocalContext.current
-
-    val img = BitmapFactory.decodeResource(context.resources, R.drawable.nitro_wallpaper_5000x2813)
-
-    val bitmap = remember {
-        mutableStateOf(img)
-    }
-
-
-
-    val scaleBitmap = bitmap.value.scale(1024, 1024, true)
-
-
+    var itemName by remember { mutableStateOf("") }
+    var itemPrice by remember { mutableStateOf("") }
+    var selectedItemCategory by remember { mutableStateOf<String?>(null) }
+    var itemDescription by remember { mutableStateOf(TextFieldValue()) }
+    var expanded by remember { mutableStateOf(false) }
+    var isChooseImage by remember { mutableStateOf(false) }
     val launcher =
         rememberLauncherForActivityResult(
             contract = ActivityResultContracts.TakePicturePreview()
         ) {
-            if (it != null) {
-                bitmap.value = it
-            }
+
         }
     val launchImage = rememberLauncherForActivityResult(
         contract = ActivityResultContracts.GetContent()
     ) {
-        if (Build.VERSION.SDK_INT < 28) {
-            bitmap.value = MediaStore.Images.Media.getBitmap(context.contentResolver, it)
-        } else {
-            val source = it?.let { it1 ->
-                ImageDecoder.createSource(context.contentResolver, it1)
-            }
-            bitmap.value = source?.let { it1 -> ImageDecoder.decodeBitmap(it1) }!!
-        }
+
     }
-    Column(
-        modifier = modifier
-            .fillMaxSize(),
-        horizontalAlignment = Alignment.CenterHorizontally,
-        verticalArrangement = Arrangement.Center
+
+    Surface(
+        modifier = Modifier.fillMaxSize()
     ) {
-
-        OutlinedTextField(
-            value = productAddUiState.productName,
-            onValueChange = productAddViewModel::updateProductName ,
-            modifier = Modifier.fillMaxWidth(),
-            label = {
-                Text(text = stringResource(id = R.string.product_name))
-            }
-        )
-        OutlinedTextField(value = productAddUiState.productQuantity.toString(),
-            onValueChange = productAddViewModel::updateProductQuantity,
-            modifier = Modifier.fillMaxWidth(),
-            label = {
-                Text(text = stringResource(id = R.string.product_quantity))
-
-            }
-        )
-        OutlinedButton(
-            onClick =  productAddViewModel::onClickAddImage ,
-            modifier = Modifier.fillMaxWidth(),
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(16.dp)
         ) {
-            Text(text = "Add image")
-        }
-
-        OutlinedTextField(
-            value = productAddUiState.productPrice.toString(),
-            onValueChange = productAddViewModel::updateProductPrice,
-            modifier = Modifier.fillMaxWidth(),
-            label = {
-                Text(text = stringResource(id = R.string.product_price))
-            },
-            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Decimal)
-        )
-        OutlinedTextField(value = productAddUiState.productDescription,
-            onValueChange = productAddViewModel::updateProductDescription,
-            modifier = Modifier.fillMaxWidth(),
-            label = {
-                Text(text = stringResource(id = R.string.product_desc))
+            Row() {
+                Icon(Icons.Filled.ArrowBackIosNew, contentDescription = null)
+                Spacer(modifier = Modifier.width(18.dp))
+                Text(
+                    text = "Add Product",
+                    style = MaterialTheme.typography.titleLarge,
+                    modifier = Modifier.padding(bottom = 16.dp)
+                )
 
             }
-        )
 
-            Image(bitmap = scaleBitmap.asImageBitmap() , contentDescription = "" )
+            TextField(
+                value = itemName,
+                onValueChange = { itemName = it },
+                label = { Text("Item Name") },
+                modifier = Modifier.fillMaxWidth()
+            )
 
+            Spacer(modifier = Modifier.height(16.dp))
 
-        Spacer(modifier = Modifier.weight(1f))
+            TextField(
+                value = itemPrice,
+                onValueChange = { itemPrice = it },
+                label = { Text("Item Price") },
+                modifier = Modifier.fillMaxWidth()
+            )
 
-        OutlinedButton(onClick = {}) {
-            Text(text = stringResource(id = R.string.add_product))
-        }
+            Spacer(modifier = Modifier.height(16.dp))
 
-        if (productAddViewModel.isChooseImage) {
-            Box(
-                modifier = Modifier
-                    .fillMaxSize()
-                    .background(Color.Black.copy(alpha = 0.5f))
+            TextField(
+                value = itemDescription,
+                onValueChange = { itemDescription = it },
+                label = { Text("Item Description") },
+                modifier = Modifier.fillMaxWidth()
+            )
+            Spacer(modifier = Modifier.height(16.dp))
+
+            OutlinedButton(
+                onClick = { isChooseImage = !isChooseImage },
+                modifier = Modifier.fillMaxWidth(),
             ) {
-                Column(
+                Text(text = "Add image")
+            }
+            Spacer(modifier = Modifier.height(16.dp))
+
+            Row(
+                verticalAlignment = Alignment.CenterVertically,
+                modifier = Modifier.clickable { expanded = !expanded }
+            ) {
+                Text(
+                    text = selectedItemCategory ?: "Select Category",
+                    modifier = Modifier.padding(end = 8.dp)
+                )
+                Icon(
+                    imageVector = Icons.Default.KeyboardArrowDown,
+                    contentDescription = "Dropdown Icon",
+                    modifier = Modifier.size(24.dp)
+                )
+            }
+
+            DropdownMenu(
+                expanded = expanded,
+                onDismissRequest = { expanded = false },
+                modifier = Modifier.fillMaxWidth()
+            ) {
+                listOf("Category 1", "Category 2", "Category 3").forEach { category ->
+                    DropdownMenuItem(
+                        text = category,
+                        onClick = {
+                            selectedItemCategory = category
+                            expanded = false
+                        }
+                    )
+                }
+            }
+
+            Spacer(modifier = Modifier.weight(1f))
+
+            Button(
+                onClick = {
+                    // TODO: Add logic to save item to database or data source
+                },
+                modifier = Modifier.fillMaxWidth()
+            ) {
+                Text("Add product")
+            }
+
+            if (isChooseImage) {
+                Box(
                     modifier = Modifier
                         .fillMaxSize()
-                        .padding(16.dp),
-                    verticalArrangement = Arrangement.Center,
-                    horizontalAlignment = Alignment.CenterHorizontally
+                        .background(Color.Black.copy(alpha = 0.5f))
                 ) {
-                    Row(
-                        verticalAlignment = Alignment.CenterVertically,
-                        horizontalArrangement = Arrangement.SpaceAround,
+                    Column(
                         modifier = Modifier
-                            .fillMaxWidth()
-                            .clip(RoundedCornerShape(16.dp))
-                            .background(Color.White)
-                            .padding(vertical = 24.dp)
+                            .fillMaxSize()
+                            .padding(16.dp),
+                        verticalArrangement = Arrangement.Center,
+                        horizontalAlignment = Alignment.CenterHorizontally
                     ) {
-                        Column(
-                            verticalArrangement = Arrangement.Center,
-                            horizontalAlignment = Alignment.CenterHorizontally
+                        Row(
+                            verticalAlignment = Alignment.CenterVertically,
+                            horizontalArrangement = Arrangement.SpaceAround,
+                            modifier = Modifier
+                                .fillMaxWidth()
+                                .clip(RoundedCornerShape(16.dp))
+                                .background(Color.White)
+                                .padding(vertical = 24.dp)
                         ) {
-                            IconButton(onClick = {
-                                launcher.launch()
-                                productAddViewModel.isChooseImage = false
-                            }) {
-                                Icon(
-                                    painter = painterResource(id = R.drawable.adidas),
-                                    contentDescription = "Camera",
-                                    modifier = Modifier.size(50.dp)
+                            Column(
+                                verticalArrangement = Arrangement.Center,
+                                horizontalAlignment = Alignment.CenterHorizontally
+                            ) {
+                                IconButton(onClick = {
+                                    launcher.launch()
+                                    isChooseImage = false
+                                }) {
+                                    Icon(
+                                        Icons.Filled.CameraAlt,
+                                        contentDescription = "Camera",
+                                        modifier = Modifier.size(50.dp)
+                                    )
+                                }
+                                Text(
+                                    text = "Camera",
+                                    fontSize = 24.sp
                                 )
                             }
-                            Text(
-                                text = "Camera",
-                                fontSize = 24.sp
-                            )
-                        }
-                        Column(
-                            verticalArrangement = Arrangement.Center,
-                            horizontalAlignment = Alignment.CenterHorizontally
-                        ) {
-                            IconButton(onClick = {
-                                launchImage.launch("image/*")
-                                productAddViewModel.isChooseImage = false
-                            }) {
-                                Icon(
-                                    painter = painterResource(id = R.drawable.adidas),
-                                    contentDescription = "Gallery",
-                                    modifier = Modifier.size(50.dp)
+                            Column(
+                                verticalArrangement = Arrangement.Center,
+                                horizontalAlignment = Alignment.CenterHorizontally
+                            ) {
+                                IconButton(onClick = {
+                                    launchImage.launch("image/*")
+                                    isChooseImage = false
+                                }) {
+                                    Icon(
+                                        Icons.Filled.AddAPhoto,
+                                        contentDescription = "Gallery",
+                                        modifier = Modifier.size(50.dp)
+                                    )
+                                }
+                                Text(
+                                    text = "Gallery",
+                                    fontSize = 24.sp
                                 )
                             }
-                            Text(
-                                text = "Gallery",
-                                fontSize = 24.sp
-                            )
                         }
                     }
                 }
@@ -224,8 +247,21 @@ fun ProductAddScreen(
     }
 }
 
+@Composable
+fun DropdownMenuItem(
+    text: String,
+    onClick: () -> Unit,
+    modifier: Modifier = Modifier,
+) {
+    Text(
+        text = text,
+        modifier = modifier
+            .clickable { onClick() }
+            .padding(vertical = 8.dp, horizontal = 16.dp)
+    )
+}
 
-@Preview(showBackground = true)
+@Preview(showSystemUi = true)
 @Composable
 fun ProductAddPreview() {
     ProductAddScreen()
